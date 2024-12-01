50f1d46ef0cfe1ba9d3e7e6d27c6c5f9b38123b6
Shubham Rajput <rajput.shubh1993@gmail.com>
2024-12-01 21:39:33 +0530

    Multiple Reducer

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const redux = require('redux')
 const createStore = redux.createStore
 
 const BUY_CAKE = 'BUY_CAKE'
+const BUY_ICECREAM = 'BUY_ICECREAM'
 
 function buyCake(){
   return{
@@ -10,13 +11,21 @@ function buyCake(){
   }
 }
 
+function buyIceCream(){
+  return{
+    type: BUY_ICECREAM
+  }
+}
+
 const initialState = {
-  numOfCakes: 10
+  numOfCakes: 10,
+  numOfIceCreams: 20
 }
 
 const reducer = (state=initialState, action) => {
   switch(action.type){
     case BUY_CAKE: return { ...state, numOfCakes: state.numOfCakes - 1 }
+    case BUY_ICECREAM: return { ...state, numOfIceCreams: state.numOfIceCreams - 1 }
     default: return state
   }
 }
@@ -27,4 +36,7 @@ const unsubscribe = store.subscribe(() => console.log('Updated State', store.get
 store.dispatch(buyCake())
 store.dispatch(buyCake())
 store.dispatch(buyCake())
+store.dispatch(buyIceCream())
+store.dispatch(buyIceCream())
+store.dispatch(buyIceCream())
 unsubscribe()
