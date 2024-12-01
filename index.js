ee4efecbd5658ca9f97c22e3f3e0c1e21c1461c8
Shubham Rajput <rajput.shubh1993@gmail.com>
2024-12-01 21:56:39 +0530

    Multiple Reducers using Combine reducers

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const redux = require('redux')
 const createStore = redux.createStore
+const combineReducers = redux.combineReducers
 
 const BUY_CAKE = 'BUY_CAKE'
 const BUY_ICECREAM = 'BUY_ICECREAM'
@@ -17,20 +18,33 @@ function buyIceCream(){
   }
 }
 
-const initialState = {
-  numOfCakes: 10,
+const initialCakeState = {
+  numOfCakes: 10
+}
+
+const initialIceCreamState = {
   numOfIceCreams: 20
 }
 
-const reducer = (state=initialState, action) => {
+const cakeReducer = (state=initialCakeState, action) => {
   switch(action.type){
     case BUY_CAKE: return { ...state, numOfCakes: state.numOfCakes - 1 }
+    default: return state
+  }
+}
+
+const iceCreamReducer = (state=initialIceCreamState, action) => {
+  switch(action.type){
     case BUY_ICECREAM: return { ...state, numOfIceCreams: state.numOfIceCreams - 1 }
     default: return state
   }
 }
 
-const store = createStore(reducer)
+const rootReducer = combineReducers({
+  cake: cakeReducer,
+  iceCream: iceCreamReducer
+})
+const store = createStore(rootReducer)
 console.log('Initial State', store.getState())
 const unsubscribe = store.subscribe(() => console.log('Updated State', store.getState()))
 store.dispatch(buyCake())
